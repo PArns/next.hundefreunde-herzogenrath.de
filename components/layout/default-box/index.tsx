404bea8eccceede2fc9d75e7f161e0022cbf4e64
Patrick Arns <github@patrick-arns.de>
2024-06-25 18:42:16 +0200

    Added drop shadow to ContentBox

--- components/layout/default-box/index.tsx
+++ components/layout/default-box/index.tsx
@@ -6,7 +6,9 @@ export default function ContentBox({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`${className} my-4 w-full rounded-lg bg-white p-4`}>
+    <div
+      className={`${className} my-4 w-full rounded-lg bg-white p-4 drop-shadow-lg`}
+    >
       {children}
     </div>
   );
